3bd46ec536c2f43f430d955fa838fdb18fd711f6
cengizygz <cengiz.1072@gmail.com>
2022-07-23 10:28:57 +0300

    init

--- lib/home.dart
+++ lib/home.dart
@@ -41,6 +41,142 @@ class _homePageState extends State<homePage> {
     );
   }
 
+  Padding score(double score) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.transparent),
+              borderRadius: BorderRadius.circular(25),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 0.5,
+                  offset: const Offset(0, 1),
+                )
+              ]),
+          width: 80,
+          height: 25,
+          child: Row(
+            children: [
+              const Icon(Icons.star, color: Color.fromARGB(255, 240, 192, 34)),
+              Text(
+                "$score / 10",
+                style: const TextStyle(
+                    color: Color.fromARGB(255, 255, 0, 0), fontSize: 16),
+              ),
+            ],
+          )),
+    );
+  }
+
+  Container category(categoryText, bool ticked) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: InkWell(
+          child: Container(
+            width: 120,
+            decoration: BoxDecoration(
+                color: ticked == true
+                    ? Color.fromARGB(255, 255, 0, 0)
+                    : Colors.white,
+                border: Border.all(color: Colors.transparent, width: 5),
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 0.5,
+                    offset: const Offset(0, 1),
+                  )
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(categoryText),
+                ticked == true
+                    ? Icon(Icons.cancel, color: Colors.white)
+                    : Icon(Icons.arrow_drop_down_circle,
+                        color: Color.fromARGB(255, 255, 0, 0))
+              ],
+            ),
+          ),
+          onTap: () {
+            Navigator.pushReplacementNamed(context, 'tum_urunler');
+          },
+        ),
+      ),
+    );
+  }
+
+  Container item(BuildContext context, String photo, String company) {
+    return Container(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+        child: Column(
+          children: [
+            InkWell(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.transparent),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                        offset: const Offset(0, 1.5),
+                      )
+                    ]),
+                height: MediaQuery.of(context).size.height * 0.22,
+                width: MediaQuery.of(context).size.height * 0.22,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, 'tum_urunler');
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "$company",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  SizedBox search(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(width: 0, color: Colors.red),
+              borderRadius: BorderRadius.all(
+                Radius.circular(05.0),
+              ),
+            ),
+            hintText: 'Neye ihtiyacın var?',
+            prefixIcon: Icon(Icons.search),
+          ),
+        ),
+      ),
+    );
+  }
+
   Container logo() {
     return Container(
       decoration: BoxDecoration(
